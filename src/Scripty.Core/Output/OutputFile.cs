dece2e1e0a7bf08eba1029b17103c2875e760876
Stefan Over <herdosratos@googlemail.com>
2016-06-21 21:31:26 +0200

    Proposed implementation for daveaglick/Scripty#15
    TODOs:
    - use TotalIndention in Write methods of OutputFile
    - might want to cache TotalIndention

--- src/Scripty.Core/Output/OutputFile.cs
+++ src/Scripty.Core/Output/OutputFile.cs
@@ -3,9 +3,21 @@ using System.IO;
 namespace Scripty.Core.Output
 {
     public abstract class OutputFile : TextWriter, IOutputFileInfo
-    {
+    {		
+		protected IIndention Indention { get; }
+
+        protected OutputFile()
+        {
+            Indention = new Indention();
+        }
+
         public abstract string FilePath { get; }
 
         public abstract BuildAction BuildAction { get; set; }
+
+        public IIndention Indent()
+        {
+            return Indention.Indent();
+        }
     }
 }
