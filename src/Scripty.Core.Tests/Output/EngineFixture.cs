bb7f36830389eb6a0164d7d8957667bb7d7f2a74
reduckted <reduckted@outlook.com>
2017-01-26 10:57:55 +1000

    Added basic tests for project and solution loading.

---
+++ src/Scripty.Core.Tests/Output/EngineFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Scripty.Core.Tests.Output
+{
+    [TestFixture]
+    public class EngineFixture
+    {
+        static readonly string SolutionFilePath = Path.GetFullPath($"{AppDomain.CurrentDomain.BaseDirectory}/../../SampleSolution/Sample.sln");
+        static readonly string ProjectFilePath = Path.GetFullPath($"{AppDomain.CurrentDomain.BaseDirectory}/../../SampleSolution/ProjA/ProjA.csproj");
+
+        [Test]
+        public void TestProjectLoading()
+        {
+            var engine = new ScriptEngine(ProjectFilePath);
+
+            Assert.IsNull(engine.ProjectRoot.Analysis.Solution.FilePath);
+            Assert.AreEqual(engine.ProjectRoot.Analysis.FilePath, ProjectFilePath);
+        }
+
+        [Test]
+        public void TestSolutionLoading()
+        {
+            var engine = new ScriptEngine(ProjectFilePath, SolutionFilePath);
+
+            Assert.AreEqual(engine.ProjectRoot.Analysis.Solution.FilePath, SolutionFilePath);
+            Assert.AreEqual(engine.ProjectRoot.Analysis.FilePath, ProjectFilePath);
+        }
+    }
+}
