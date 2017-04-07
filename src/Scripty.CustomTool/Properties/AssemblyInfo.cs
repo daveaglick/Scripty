8dbf5f0b7af8e3818974c68e25dd9f38a3758f97
Dave Glick <dave@daveaglick.com>
2017-04-07 17:01:12 -0400

    Attempting to resolve binding failures with custom tool (#89)

--- src/Scripty.CustomTool/Properties/AssemblyInfo.cs
+++ src/Scripty.CustomTool/Properties/AssemblyInfo.cs
@@ -1,8 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
 
 [assembly: AssemblyTitle("Scripty.CustomTool")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("dd26cc89-e1e8-4a94-8e8c-ad5215616c6c")]
 [assembly: ComVisible(true)]
+
+[assembly: ProvideBindingRedirection(AssemblyName = "System.IO.FileSystem", PublicKeyToken = "b03f5f7f11d50a3a",
+    OldVersionLowerBound = "0.0.0.0", OldVersionUpperBound = "4.0.2.0", NewVersion = "4.0.2.0")]
+
+[assembly: ProvideBindingRedirection(AssemblyName = "System.IO.FileSystem.Primitives", PublicKeyToken = "b03f5f7f11d50a3a",
+    OldVersionLowerBound = "0.0.0.0", OldVersionUpperBound = "4.0.2.0", NewVersion = "4.0.2.0")]
